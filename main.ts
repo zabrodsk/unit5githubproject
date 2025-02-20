a48c5821fb2e62150d0fed6c437a6708bb750ce7
zabrodsk <dusan.zabrodsky@gmail.com>
2025-02-20 08:18:03 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -20,3 +20,23 @@ WaterBoy = sprites.create(img`
     . . . . 9 9 . . . 9 9 . . . . . 
     . . . 9 9 9 9 . . . 9 9 . . . . 
     `, SpriteKind.Player)
+controller.moveSprite(WaterBoy)
+let FireGirl = sprites.create(img`
+    . . 2 . 2 . . 2 . . 2 . . . . . 
+    . 2 . . 2 . . 2 . . 2 . . . . . 
+    . 2 2 . 2 . . 2 . . 2 . . . . . 
+    . . 2 2 2 2 2 2 2 2 2 . . . . . 
+    . . 2 f 2 f 2 f 2 f 2 . . . . . 
+    . . 2 f 2 2 2 2 2 f 2 . . . . . 
+    . . 2 f f f f f f f 2 . . . . . 
+    . . 2 2 2 2 2 2 2 2 2 . . . . . 
+    2 . . . . 2 2 2 2 . . . . 2 2 . 
+    . 2 . . . 2 2 2 . . . 2 2 . . . 
+    . . 2 . 2 2 2 2 2 . 2 . . . . . 
+    . . . 2 2 2 2 2 2 2 . . . . . . 
+    . . . 2 2 2 2 2 2 2 2 . . . . . 
+    . . 2 2 2 2 2 2 2 2 2 2 2 . . . 
+    . 2 2 2 2 2 2 2 2 2 2 2 2 . . . 
+    . . . . 2 . . . . 2 . . . . . . 
+    `, SpriteKind.Player)
+controller.player2.moveSprite(FireGirl)
