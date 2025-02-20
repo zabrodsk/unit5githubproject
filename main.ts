3025ef993d66137429c50d6dee23d01eb3af335e
zabrodsk <dusan.zabrodsky@gmail.com>
2025-02-20 08:13:39 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,22 @@
-
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    scene.cameraFollowSprite(WaterBoy)
+})
+let WaterBoy: Sprite = null
+WaterBoy = sprites.create(img`
+    . . . . 9 9 9 9 9 9 9 . . . . . 
+    . . . . 9 9 f 9 f 9 9 . . . . . 
+    . . . 9 9 f 9 9 9 f 9 9 . . . . 
+    . . . . 9 9 f f f 9 9 . . . . . 
+    . . . . 9 9 9 9 9 9 9 . . . . . 
+    9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 . 
+    9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 . 
+    9 9 . . . 9 9 9 9 9 9 . . 9 9 . 
+    9 . . . . 9 9 9 9 9 . . . 9 9 . 
+    9 9 . . . 9 9 9 9 . . . . 9 9 . 
+    9 9 . . . . 9 . 9 . . . . 9 9 . 
+    9 9 . . . 9 9 . 9 . . . . 9 9 . 
+    . . . . . 9 . . 9 9 . . . . . . 
+    . . . . . 9 . . . 9 . . . . . . 
+    . . . . 9 9 . . . 9 9 . . . . . 
+    . . . 9 9 9 9 . . . 9 9 . . . . 
+    `, SpriteKind.Player)
