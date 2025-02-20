c591107cd76fbb409e6c1b1f7e9c9dcd99640ae9
zabrodsk <dusan.zabrodsky@gmail.com>
2025-02-20 08:36:18 +0100

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,42 +1,58 @@
+controller.player2.onButtonEvent(ControllerButton.B, ControllerButtonEvent.Pressed, function () {
+    scene.cameraFollowSprite(FireGirl)
+})
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    scene.cameraFollowSprite(FireGirl)
+})
+function level1 () {
+    WaterBoy = sprites.create(img`
+        . . . . 9 9 9 9 9 9 9 . . . . . 
+        . . . . 9 9 f 9 f 9 9 . . . . . 
+        . . . 9 9 f 9 9 9 f 9 9 . . . . 
+        . . . . 9 9 f f f 9 9 . . . . . 
+        . . . . 9 9 9 9 9 9 9 . . . . . 
+        9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 . 
+        9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 . 
+        9 9 . . . 9 9 9 9 9 9 . . 9 9 . 
+        9 . . . . 9 9 9 9 9 . . . 9 9 . 
+        9 9 . . . 9 9 9 9 . . . . 9 9 . 
+        9 9 . . . . 9 . 9 . . . . 9 9 . 
+        9 9 . . . 9 9 . 9 . . . . 9 9 . 
+        . . . . . 9 . . 9 9 . . . . . . 
+        . . . . . 9 . . . 9 . . . . . . 
+        . . . . 9 9 . . . 9 9 . . . . . 
+        . . . 9 9 9 9 . . . 9 9 . . . . 
+        `, SpriteKind.Player)
+    controller.moveSprite(WaterBoy)
+    FireGirl = sprites.create(img`
+        . . 2 . 2 . . 2 . . 2 . . . . . 
+        . 2 . . 2 . . 2 . . 2 . . . . . 
+        . 2 2 . 2 . . 2 . . 2 . . . . . 
+        . . 2 2 2 2 2 2 2 2 2 . . . . . 
+        . . 2 f 2 f 2 f 2 f 2 . . . . . 
+        . . 2 f 2 2 2 2 2 f 2 . . . . . 
+        . . 2 f f f f f f f 2 . . . . . 
+        . . 2 2 2 2 2 2 2 2 2 . . . . . 
+        2 . . . . 2 2 2 2 . . . . 2 2 . 
+        . 2 . . . 2 2 2 . . . 2 2 . . . 
+        . . 2 . 2 2 2 2 2 . 2 . . . . . 
+        . . . 2 2 2 2 2 2 2 . . . . . . 
+        . . . 2 2 2 2 2 2 2 2 . . . . . 
+        . . 2 2 2 2 2 2 2 2 2 2 2 . . . 
+        . 2 2 2 2 2 2 2 2 2 2 2 2 . . . 
+        . . . . 2 . . . . 2 . . . . . . 
+        `, SpriteKind.Player)
+    controller.player2.moveSprite(FireGirl)
+    tiles.setCurrentTilemap(tilemap`level2`)
+    tiles.placeOnRandomTile(WaterBoy, sprites.dungeon.stairEast)
+    tiles.placeOnRandomTile(FireGirl, sprites.dungeon.stairWest)
+}
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     scene.cameraFollowSprite(WaterBoy)
 })
+controller.player2.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
+    scene.cameraFollowSprite(WaterBoy)
+})
 let WaterBoy: Sprite = null
-WaterBoy = sprites.create(img`
-    . . . . 9 9 9 9 9 9 9 . . . . . 
-    . . . . 9 9 f 9 f 9 9 . . . . . 
-    . . . 9 9 f 9 9 9 f 9 9 . . . . 
-    . . . . 9 9 f f f 9 9 . . . . . 
-    . . . . 9 9 9 9 9 9 9 . . . . . 
-    9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 . 
-    9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 . 
-    9 9 . . . 9 9 9 9 9 9 . . 9 9 . 
-    9 . . . . 9 9 9 9 9 . . . 9 9 . 
-    9 9 . . . 9 9 9 9 . . . . 9 9 . 
-    9 9 . . . . 9 . 9 . . . . 9 9 . 
-    9 9 . . . 9 9 . 9 . . . . 9 9 . 
-    . . . . . 9 . . 9 9 . . . . . . 
-    . . . . . 9 . . . 9 . . . . . . 
-    . . . . 9 9 . . . 9 9 . . . . . 
-    . . . 9 9 9 9 . . . 9 9 . . . . 
-    `, SpriteKind.Player)
-controller.moveSprite(WaterBoy)
-let FireGirl = sprites.create(img`
-    . . 2 . 2 . . 2 . . 2 . . . . . 
-    . 2 . . 2 . . 2 . . 2 . . . . . 
-    . 2 2 . 2 . . 2 . . 2 . . . . . 
-    . . 2 2 2 2 2 2 2 2 2 . . . . . 
-    . . 2 f 2 f 2 f 2 f 2 . . . . . 
-    . . 2 f 2 2 2 2 2 f 2 . . . . . 
-    . . 2 f f f f f f f 2 . . . . . 
-    . . 2 2 2 2 2 2 2 2 2 . . . . . 
-    2 . . . . 2 2 2 2 . . . . 2 2 . 
-    . 2 . . . 2 2 2 . . . 2 2 . . . 
-    . . 2 . 2 2 2 2 2 . 2 . . . . . 
-    . . . 2 2 2 2 2 2 2 . . . . . . 
-    . . . 2 2 2 2 2 2 2 2 . . . . . 
-    . . 2 2 2 2 2 2 2 2 2 2 2 . . . 
-    . 2 2 2 2 2 2 2 2 2 2 2 2 . . . 
-    . . . . 2 . . . . 2 . . . . . . 
-    `, SpriteKind.Player)
-controller.player2.moveSprite(FireGirl)
+let FireGirl: Sprite = null
+level1()
---
+++ tilemap.g.ts
@@ -0,0 +1,58 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16], TileScale.Sixteen);
+            case "level2":
+            case "level2":return tiles.createTilemap(hex`100010000303030101030303030303010303030301111111111111111111111111111104031111111111111111111111111111010311111111111111110d0211111111030111111111111111110802111111110303111111111111111108021111111101031111111111111111080211090d1101011111111111111111080211100811030311111111111111110802111008110303090a0e0a0e0a0d110802111008110301111111111111081108021110081103031111090d11110811080211100811030111110c1111110811080211100811030311110c0811110f11080211100811030511110c08111111110802070b12110603020201020202020102010202020203`, img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 . . . . . . . . . . . . . . . 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+. . . . . . . . . . . . . . . . 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,sprites.dungeon.floorDark1,sprites.dungeon.floorDark0,sprites.dungeon.floorDark4,sprites.dungeon.stairLarge,sprites.dungeon.stairEast,sprites.dungeon.stairWest,sprites.dungeon.stairLadder,sprites.dungeon.purpleOuterEast0,sprites.dungeon.purpleOuterNorthWest,sprites.dungeon.purpleOuterNorth1,sprites.dungeon.purpleOuterSouthEast,sprites.dungeon.purpleOuterWest0,sprites.dungeon.purpleOuterNorthEast,sprites.dungeon.purpleOuterNorth2,sprites.dungeon.purpleOuterEast2,sprites.dungeon.purpleOuterWest1,sprites.dungeon.darkGroundCenter,sprites.dungeon.purpleOuterSouthWest], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
